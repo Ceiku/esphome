ab39043b26f876b2afa0140b9684a1099769cbae
Daniel Steen-Brungot <daniel.steenbrungot@gmail.com>
2020-09-16 14:56:05 +0200

    Update esphome/components/adcp/adcp.cpp
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/adcp/adcp.cpp
+++ esphome/components/adcp/adcp.cpp
@@ -10,11 +10,17 @@ void ADCPComponent::dump_config() { ESP_LOGCONFIG(TAG, "Setting up ADCP..."); }
 
 void ADCPSensor::update() {
   this->power_pin_->digital_write(true);
-  delay(this->delay_);
-  float state = this->parent_->get_source()->sample();
-  this->power_pin_->digital_write(false);
-  this->publish_state(state);
+  this->read_timestamp_ = millis();
+  this->read_ = true;
 }
 
+void ADCPSensor::loop() {
+  if (this->read_ && millis() > this->read_timestamp_ + this->delay_) {
+    float state = this->parent_->get_source()->sample();
+    this->power_pin_->digital_write(false);
+    this->publish_state(state);
+    this->read_ = false;
+  }
+}
 }  // namespace adcp
 }  // namespace esphome
