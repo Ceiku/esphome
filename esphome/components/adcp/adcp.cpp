e703c8daf9e33361da0c0106fee6114a86b8f9b0
Daniel Steen-Brungot <daniel.steenbrungot@gmail.com>
2020-09-17 13:53:52 +0200

    Update esphome/components/adcp/adcp.cpp
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/adcp/adcp.cpp
+++ esphome/components/adcp/adcp.cpp
@@ -13,7 +13,7 @@ void ADCPSensor::update() {
   this->set_timeout((uint32_t) this->delay_, [this](){ this->complete_update_(); });
 }
 
-void ADCPSensor::complete_update() {
+void ADCPSensor::complete_update_() {
   float state = this->parent_->get_source()->sample();
   this->power_pin_->digital_write(false);
   this->publish_state(state);
