741c905724b094a7003cc1bd89fb2cc78b31e739
Ceiku <daniel.steenbrungot@gmail.com>
2020-09-22 22:39:12 +0000

    Add MVP for tfmicro sensor with AllOpsResolver, Commit for debugging failed assertion '.' not in domain

---
+++ esphome/components/tfmicro/sensor.py
@@ -0,0 +1,48 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import CONF_SENSOR, CONF_ID, UNIT_VOLT, ICON_FLASH, ESP_PLATFORM_ESP32
+
+# DEPENDENCIES = ['sensor']
+# ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
+
+# MULTI_CONF = True
+IS_PLATFORM_COMPONENT = True
+
+CONF_INPUT_SIZE = 'input_size'
+CONF_OUTPUT_SIZE = 'output_size'
+CONF_TENSOR_ARENA_SIZE = 'tensor_arena_size'
+CONF_RESOLVERS = 'resolvers'
+
+tfmicro_ns = cg.esphome_ns.namespace('tfmicro')
+TFMicroSensor = tfmicro_ns.class_('TFMicroSensor', sensor.Sensor, cg.PollingComponent)
+
+CONFIG_SCHEMA = sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 3).extend({
+    cv.GenerateID(): cv.declare_id(TFMicroSensor),
+    cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
+    cv.Required(CONF_MODEL): cv.ensure_list(cv.hex_uint8_t),
+    
+    cv.Optional(CONF_RESOLVERS, default=[]): cv.ensure_list(cv.string_strict),
+    cv.Optional(CONF_INPUT_SIZE, default=1): cv.positive_not_null_int,
+    cv.Optional(CONF_OUTPUT_SIZE, default=1): cv.positive_not_null_int,
+    cv.Optional(CONF_TENSOR_ARENA_SIZE, default=2048): cv.positive_not_null_int,
+}).extend(cv.polling_component_schema('60s'))
+
+def to_code(config):
+  template = cg.TemplateArguments(config[CONF_INPUT_SIZE], config[CONF_OUTPUT_SIZE], config[CONF_TENSOR_ARENA_SIZE])
+
+  tfm_type = TFMicroSensor.template(template)
+  rhs = tfm_type.new()
+
+  var = cg.new_Pvariable(config[CONF_ID], rhs, tfm_type)
+  yield cg.register_component(var, config)
+  yield sensor.register_sensor(var, config)
+
+  sens = yield cg.get_variable(config[CONF_SENSOR])
+  resolvers = yield cg.get_variable(config[CONF_RESOLVERS])
+  
+  cg.add(var.set_sensor(sens))
+  cg.add(var.set_resolvers(resolvers))
+  cg.add_library('EloquentTinyML', '0.0.2')
+
+    
---
+++ esphome/components/tfmicro/tfmicro.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <EloquentTinyML.h>
+#include "esphome/core/component.h"
+#include "esphome/core/esphal.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace tfmicro {
+
+template<size_t inputSize, size_t outputSize, size_t tensorArenaSize>
+class TFMicroSensor : public sensor::Sensor, public PollingComponent {
+ public:
+  void setup() override;
+  void update() override;
+
+  void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
+  void set_model(unsigned char *model) { this->model_ = model; }
+  void set_resolvers(std::string *resolvers) { this->resolvers_ = resolvers; }
+  
+  void set_ml(size_t inputSize, size_t outputSize, size_t tensorArenaSize) { this->set_ml(new Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize>()); }
+  void set_ml(Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize> *ml) { this->ml_ = ml; }
+
+ protected:
+  Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize> *ml_{nullptr};;
+  sensor::Sensor *sensor_;
+  unsigned char *model_;
+  std::string *resolvers_;
+};
+
+}  // namespace tfmicro
+}  // namespace esphome
