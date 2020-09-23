39011fd012aee65633894f9b1d430c0c99e9dd23
Ceiku <daniel.steenbrungot@gmail.com>
2020-09-23 22:30:14 +0000

    Fix some compiler bugs with setting template values, need to validate the model

--- esphome/components/tfmicro/sensor.py
+++ esphome/components/tfmicro/sensor.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_SENSOR, CONF_ID, UNIT_PERCENT, ICON_BRAIN, ESP_PLATFORM_ESP32, CONF_MODEL
+# from esphome.py_compat import text_type, binary_type, char_to_byte
 
 DEPENDENCIES = ['sensor']
 ESP_PLATFORMS = [ESP_PLATFORM_ESP32]
@@ -16,14 +17,24 @@ CONF_RESOLVERS = 'resolvers'
 tfmicro_ns = cg.esphome_ns.namespace('tfmicro')
 TFMicroSensor = tfmicro_ns.class_('TFMicroSensor', sensor.Sensor, cg.PollingComponent)
 
+
+# def validate_data(value):
+#     if isinstance(value, text_type):
+#         return value.encode('utf-8')
+#     if isinstance(value, str):
+#         return value
+#     if isinstance(value, list):
+#         return cv.Schema([cv.char])(value)
+#     raise cv.Invalid("data must either be a string wrapped in quotes or a list of bytes")
+
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_PERCENT, ICON_BRAIN, 3).extend({
     cv.GenerateID(): cv.declare_id(TFMicroSensor),
     cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
-    cv.Required(CONF_MODEL): cv.ensure_list(cv.hex_uint8_t),
+    cv.Required(CONF_MODEL):  cv.ensure_list(cv.All(cv.unsigned, cv.char)),
     cv.Optional(CONF_RESOLVERS, default='all'): cv.ensure_list(cv.string_strict),
-    cv.Optional(CONF_INPUT_SIZE, default=1): cv.positive_not_null_int,
-    cv.Optional(CONF_OUTPUT_SIZE, default=1): cv.positive_not_null_int,
-    cv.Optional(CONF_TENSOR_ARENA_SIZE, default=2048): cv.positive_not_null_int,
+    cv.Optional(CONF_INPUT_SIZE, default=1): cv.All(cv.uint8_t, cv.positive_not_null_int),
+    cv.Optional(CONF_OUTPUT_SIZE, default=1): cv.All(cv.uint8_t, cv.positive_not_null_int),
+    cv.Optional(CONF_TENSOR_ARENA_SIZE, default=2048): cv.All(cv.uint8_t, cv.positive_not_null_int),
 }).extend(cv.polling_component_schema('60s'))
 
 def to_code(config):
@@ -31,15 +42,19 @@ def to_code(config):
 
   tfm_type = TFMicroSensor.template(template)
   rhs = tfm_type.new()
-
   var = cg.new_Pvariable(config[CONF_ID], rhs, tfm_type)
+  
   yield cg.register_component(var, config)
   yield sensor.register_sensor(var, config)
 
   sens = yield cg.get_variable(config[CONF_SENSOR])
+
+  model = config[CONF_MODEL]
+    # if isinstance(model, binary_type):
+    #     data = [HexInt(char_to_byte(x)) for x in data]
+    # cg.add(var.set_data(model))
   
   cg.add(var.set_sensor(sens))
   cg.add(var.set_resolvers(config[CONF_RESOLVERS]))
   cg.add_library('EloquentTinyML', '0.0.2')
 
-    
--- esphome/components/tfmicro/tfmicro.h
+++ esphome/components/tfmicro/tfmicro.h
@@ -11,15 +11,22 @@ namespace tfmicro {
 template<size_t inputSize, size_t outputSize, size_t tensorArenaSize>
 class TFMicroSensor : public sensor::Sensor, public PollingComponent {
  public:
-  void setup() override;
-  void update() override;
-
   void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
   void set_model(unsigned char *model) { this->model_ = model; }
   void set_resolvers(std::string *resolvers) { this->resolvers_ = resolvers; }
   
-  void set_ml(size_t inputSize, size_t outputSize, size_t tensorArenaSize) { this->set_ml(new Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize>()); }
-  void set_ml(Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize> *ml) { this->ml_ = ml; }
+  // void set_ml() { this->set_ml(new Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize>()); }
+  // void set_ml(Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize> *ml) { this->ml_ = ml; }
+
+  void setup() override { 
+    // this->set_ml();
+    this->ml->begin(this->model_);
+  }
+
+  void update() override {
+    float predicted = this->ml->predict({ this->sensor_->sample() });
+    this->publish_state(predicted);
+  }
 
  protected:
   Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize> *ml_{nullptr};;
