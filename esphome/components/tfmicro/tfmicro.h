1f58aee1951933a54c2abe51c79c7d6b1118d4a6
Ceiku <daniel.steenbrungot@gmail.com>
2020-09-25 13:06:00 +0000

    tfmicro sets model from bytes

--- esphome/components/tfmicro/tfmicro.h
+++ esphome/components/tfmicro/tfmicro.h
@@ -11,22 +11,22 @@ namespace tfmicro {
 template<size_t inputSize, size_t outputSize, size_t tensorArenaSize>
 class TFMicroSensor : public sensor::Sensor, public PollingComponent {
  public:
-  template<size_t inputSize, size_t outputSize, size_t tensorArenaSize>
-  TFMicroSensor<inputSize, outputSize, tensorArenaSize>::TFMicroSensor() {}
+  // template<size_t inputSize, size_t outputSize, size_t tensorArenaSize>
+  // TFMicroSensor<inputSize, outputSize, tensorArenaSize>::TFMicroSensor() {}
   void set_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
   void set_model(unsigned char *model) { this->model_ = model; }
   void set_resolvers(std::string *resolvers) { this->resolvers_ = resolvers; }
 
-  // void set_ml() { this->ml_ = new Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize>(); }
+  void set_ml() { this->ml_ = new Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize>(); }
   void set_ml(Eloquent::TinyML::TfLite<inputSize, outputSize, tensorArenaSize> *ml) { this->ml_ = ml; }
 
   void setup() override { 
-    // this->ml->begin(this->model_);
+    this->ml->begin(this->model_);
   }
 
   void update() override {
-    // auto predicted = this->ml->predict({ this->sensor_->sample() });
-    // this->publish_state(predicted);
+    auto predicted = this->ml->predict({ this->sensor_->sample() });
+    this->publish_state(predicted);
   }
 
  protected:
